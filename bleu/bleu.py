1e505fb73a29171946fb236882ad92a59807f098
Joseph Palermo <josephwpalermo@gmail.com>
2017-09-12 12:22:30 -0400

    implement and test full-scale bleu

--- bleu/bleu.py
+++ bleu/bleu.py
@@ -1,8 +1,14 @@
+import math
+
 # genenral utilities
 
 # preprocess an input sentence
 def preproc(sentence, n):
     return ngramify(sentence.lower().split(), n)
+
+# preprocess a list of sentences
+def preproc_list(sentence_list, n):
+    return [preproc(sentence, n) for sentence in sentence_list]
 
 # construct a list of all n-grams in the sentence
 def ngramify(sentence, n):
@@ -13,6 +19,7 @@ def ngramify(sentence, n):
 def count(word, words):
     return sum([1 if word == w else 0 for w in words])
 
+# construct a set which is the union of a list of sets
 def union_of_sets(sets):
     return set().union(*sets)
 
@@ -20,6 +27,14 @@ def union_of_sets(sets):
 def max_clip(word, references):
     reference_counts = [count(word, ref) for ref in references]
     return max(reference_counts)
+
+# return the integer in the list 'ls' which is closest in value to the integer n
+def closest(n, ls):
+    return min([(m, abs(n - m)) for m in ls], key=lambda x: x[1])[0]
+
+# get the gemetric mean of a list
+def geometric_mean(ls):
+    return sum(map(lambda x: math.log(x), ls)) / len(ls)
 
 # utilities to compute precision values
 
@@ -35,3 +50,44 @@ def modified_precision(candidate, references):
     max_clips = [max_clip(word, references) for word in candidate_words]
     score = sum([min(c, mclip) for c,mclip in zip(counts, max_clips)])
     return "modified precision, " + str(score) + "/" + str(len(candidate))
+
+def corpus_modified_precision(candidates, references_list):
+    score = 0
+    gram_len = sum([len(cand) for cand in candidates])
+    for cand, refs in zip(candidates, references_list):
+        cand_grams = set(cand)
+        for gram in cand_grams:
+            gram_count = count(gram, cand)
+            mclip = max_clip(gram, refs)
+            gram_score = min(gram_count, mclip)
+            score += gram_score
+    return score / gram_len
+
+
+# utilities to compute brevity penlaty
+
+def best_match_lengths(candidates, references_list):
+    candidate_lengths = [len(c) for c in candidates]
+    reference_lengths = [[len(r) for r in refs] for refs in references_list]
+    return [closest(candidate_lengths[i], reference_lengths[i]) for i in range(len(candidates))]
+
+def effective_reference_length(candidates, references_list):
+    return sum(best_match_lengths(candidates, references_list))
+
+def brevity_penalty(candidates, references_list):
+    r = effective_reference_length(candidates, references_list)
+    c = sum([len(cand) for cand in candidates])
+    if c > r:
+        return 1
+    else:
+        return math.exp(1 - r/c)
+
+# compute the bleu score
+
+def bleu_score(raw_candidates, raw_references_list, n):
+    candidates_list = [preproc_list(raw_candidates, i+1) for i in range(n)]
+    references_lists = [[preproc_list(raw_references, i+1) for raw_references in raw_references_list] for i in range(n)]
+    p_values = [corpus_modified_precision(cands, refs_list) for cands,refs_list in zip(candidates_list, references_lists)]
+    bp = brevity_penalty(candidates_list[0], references_lists[0])
+    p_score = math.exp(geometric_mean(p_values))
+    return bp * p_score
--- bleu/testing.py
+++ bleu/testing.py
@@ -1,59 +1,116 @@
-from bleu import preproc, precision, modified_precision
+from bleu import *
 
 """
 Testing that the results from http://www.aclweb.org/anthology/P02-1040.pdf
-(BLEU: a Method for Automatic Evaluation of Machine Translation) can be
+(BLEU: a Method for Automatic Evaluation of Machine Translation) and from
+here (http://www.nltk.org/_modules/nltk/translate/bleu_score.html) can be
 replicated
 """
 
-# unigrams
+# some unit testing
 
-print("testing unigrams")
-print("example 1")
-candidate = preproc("the the the the the the the", 1)
-ref1 = preproc("The cat is on the mat", 1)
-ref2 = preproc("There is a cat on the mat", 1)
-references = [ref1, ref2]
-print("candidate 1")
-print(precision(candidate, references))
-print(modified_precision(candidate, references))
+# print("test closest")
+assert closest(5, [4, 7, 9]) == 4
+assert closest(15, [4, 7, 9]) == 9
 
-print("example 2")
-candidate1 = preproc("It is a guide to action which ensures that the military always obeys the commands of the party", 1)
-candidate2 = preproc("It is to insure the troops forever hearing the activity guidebook that party direct", 1)
-ref1 = preproc("It is a guide to action that ensures that the military will forever heed Party commands", 1)
-ref2 = preproc("It is the guiding principle which guarantees the military forces always being under the command of the Party", 1)
-ref3 = preproc("It is the practical guide for the army always to heed the directions of the party", 1)
-references = [ref1, ref2, ref3]
-print("candidate 1")
-print(precision(candidate1, references))
-print(modified_precision(candidate1, references))
-print("candidate 2")
-print(precision(candidate2, references))
-print(modified_precision(candidate2, references))
+# visual test of n-grams
 
-# bigrams
+# test = preproc("The cat is on the mat", 1)
+# test2 = preproc("The cat is on the mat", 2)
+# print(test)
+# print(test2)
+#
+# # unigrams
+#
+# print("testing unigrams")
+# print("example 1")
+# candidate = preproc("the the the the the the the", 1)
+# ref1 = preproc("The cat is on the mat", 1)
+# ref2 = preproc("There is a cat on the mat", 1)
+# references = [ref1, ref2]
+# print("candidate 1")
+# print(precision(candidate, references))
+# print(modified_precision(candidate, references))
+#
+# print("example 2")
+# candidate1 = preproc("It is a guide to action which ensures that the military always obeys the commands of the party", 1)
+# candidate2 = preproc("It is to insure the troops forever hearing the activity guidebook that party direct", 1)
+# ref1 = preproc("It is a guide to action that ensures that the military will forever heed Party commands", 1)
+# ref2 = preproc("It is the guiding principle which guarantees the military forces always being under the command of the Party", 1)
+# ref3 = preproc("It is the practical guide for the army always to heed the directions of the party", 1)
+# references = [ref1, ref2, ref3]
+# print("candidate 1")
+# print(precision(candidate1, references))
+# print(modified_precision(candidate1, references))
+# print("candidate 2")
+# print(precision(candidate2, references))
+# print(modified_precision(candidate2, references))
+#
+# # bigrams
+#
+# print("\ntesting bigrams")
+# print("example 1")
+# candidate = preproc("the the the the the the the", 2)
+# ref1 = preproc("The cat is on the mat", 2)
+# ref2 = preproc("There is a cat on the mat", 2)
+# references = [ref1, ref2]
+# print("candidate 1")
+# print(precision(candidate, references))
+# print(modified_precision(candidate, references))
+#
+# print("example 2")
+# candidate1 = preproc("It is a guide to action which ensures that the military always obeys the commands of the party", 2)
+# candidate2 = preproc("It is to insure the troops forever hearing the activity guidebook that party direct", 2)
+# ref1 = preproc("It is a guide to action that ensures that the military will forever heed Party commands", 2)
+# ref2 = preproc("It is the guiding principle which guarantees the military forces always being under the command of the Party", 2)
+# ref3 = preproc("It is the practical guide for the army always to heed the directions of the party", 2)
+# references = [ref1, ref2, ref3]
+# print("candidate 1")
+# print(precision(candidate1, references))
+# print(modified_precision(candidate1, references))
+# print("candidate 2")
+# print(precision(candidate2, references))
+# print(modified_precision(candidate2, references))
 
-print("\ntesting bigrams")
-print("example 1")
-candidate = preproc("the the the the the the the", 2)
-ref1 = preproc("The cat is on the mat", 2)
-ref2 = preproc("There is a cat on the mat", 2)
-references = [ref1, ref2]
-print("candidate 1")
-print(precision(candidate, references))
-print(modified_precision(candidate, references))
+# full scale testing
 
-print("example 2")
-candidate1 = preproc("It is a guide to action which ensures that the military always obeys the commands of the party", 2)
-candidate2 = preproc("It is to insure the troops forever hearing the activity guidebook that party direct", 2)
-ref1 = preproc("It is a guide to action that ensures that the military will forever heed Party commands", 2)
-ref2 = preproc("It is the guiding principle which guarantees the military forces always being under the command of the Party", 2)
-ref3 = preproc("It is the practical guide for the army always to heed the directions of the party", 2)
-references = [ref1, ref2, ref3]
-print("candidate 1")
-print(precision(candidate1, references))
-print(modified_precision(candidate1, references))
-print("candidate 2")
-print(precision(candidate2, references))
-print(modified_precision(candidate2, references))
+candidates = preproc_list(["It is a guide to action which ensures that the military always obeys the commands of the party",
+                           "It is to insure the troops forever hearing the activity guidebook that party direct"], 1)
+references_list = [preproc_list(["It is a guide to action that ensures that the military will forever heed Party commands",
+                            "It is the guiding principle which guarantees the military forces always being under the command of the Party",
+                            "It is the practical guide for the army always to heed the directions of the party"], 1)]
+references_list *= 2 # since there are 2 candidates
+
+# visual test of length matching
+
+# print([len(cand) for cand in candidates])
+# print([[len(ref) for ref in refs] for refs in references_list])
+# print(best_match_lengths(candidates, references_list))
+# print("c: " + str(sum([len(cand) for cand in candidates])))
+# print("r: " + str(effective_reference_length(candidates, references_list)))
+# print("brevity penalty: " + str(brevity_penalty(candidates, references_list)))
+
+assert effective_reference_length(candidates, references_list) == 34  # 18 + 16
+
+# visual test of precision computation
+
+# print(corpus_modified_precision([candidates[0]], [references_list[0]])) # equivalent to 17/18
+# print(corpus_modified_precision([candidates[1]], [references_list[1]])) # equivalent to 8/14
+# print(corpus_modified_precision(candidates, references_list))
+
+# testing bleu
+
+raw_candidates = ["It is a guide to action which ensures that the military always obeys the commands of the party"]
+raw_references_list = [["It is a guide to action that ensures that the military will forever heed Party commands",
+                       "It is the guiding principle which guarantees the military forces always being under the command of the Party",
+                       "It is the practical guide for the army always to heed the directions of the party"]]
+print(bleu_score(raw_candidates, raw_references_list, 4))
+
+raw_candidates = ["It is a guide to action which ensures that the military always obeys the commands of the party",
+                  "he read the book because he was interested in world history"]
+
+raw_references_list = [["It is a guide to action that ensures that the military will forever heed Party commands",
+                       "It is the guiding principle which guarantees the military forces always being under the command of the Party",
+                       "It is the practical guide for the army always to heed the directions of the party"],
+                       ["he was interested in world history because he read the book"]]
+print(bleu_score(raw_candidates, raw_references_list, 4))
